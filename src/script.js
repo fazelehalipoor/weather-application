4f4892687912b58c9046e4058f8776638d7998d3
fazelehalipoor <148051136+fazelehalipoor@users.noreply.github.com>
2025-08-25 20:25:36 +0330

    added axios

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-search-input");
   let city = document.querySelector("#city-name");
   city.innerHTML = searchInput.value;
-  console.log(city);
 }
 let citySearchForm = document.querySelector("#city-search");
 citySearchForm.addEventListener("submit", handleSearchSubmit);
