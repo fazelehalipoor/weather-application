d6454f84e898d8f91890901a1b2bc27805348804
fazelehalipoor <148051136+fazelehalipoor@users.noreply.github.com>
2025-08-29 18:46:19 +0330

    added links to html

--- src/script.js
+++ src/script.js
@@ -1,9 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
 
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "21b4c3ea35c50f8b3156odtae63b894e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=Metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-search-input");
-  let city = document.querySelector("#city-name");
-  city.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
+
 let citySearchForm = document.querySelector("#city-search");
 citySearchForm.addEventListener("submit", handleSearchSubmit);
